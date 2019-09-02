83e35b951b6b5a5a83f3d6dad4c07a9d054edf35
liuzhihao <834281422@qq.com>
2019-09-02 10:14:03 +0800

    修改名字

---
+++ src/ApiResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Liuzhihao\ApiResponse;
+
+class ApiResponse
+{
+   
+    public function __construct()
+    {
+
+    }
+    public static function demo()
+    {
+    	dd('11');
+    }
+
+}
